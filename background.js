4828a677776b81ef3ca66132fea366c60ccd7d4f
Hiroaki Nakamura <hnakamur@gmail.com>
2017-09-18 10:20:42 +0900

    Add "Create submenus" option

--- background.js
+++ background.js
@@ -1,74 +1,24 @@
-function copyToClipboard(text) {
-  function oncopy(event) {
-    document.removeEventListener("copy", oncopy, true);
-    // Hide the event from the page to prevent tampering.
-    event.stopImmediatePropagation();
+async function copyToClipboard(text) {
+  return await new Promise((resolve, reject) => {
+    function oncopy(event) {
+      document.removeEventListener("copy", oncopy, true);
+      // Hide the event from the page to prevent tampering.
+      event.stopImmediatePropagation();
 
-    // Overwrite the clipboard content.
-    event.preventDefault();
-    event.clipboardData.setData("text/plain", text);
-  }
-  document.addEventListener("copy", oncopy, true);
+      // Overwrite the clipboard content.
+      event.preventDefault();
+      event.clipboardData.setData("text/plain", text);
+      resolve();
+    }
+    document.addEventListener("copy", oncopy, true);
 
-  // Requires the clipboardWrite permission, or a user gesture:
-  document.execCommand("copy");
-}
-
-function createContextMenu(options) {
-  var title = formatMenuItemTitle(options['title' + options['defaultFormat']]);
-  chrome.contextMenus.create({
-    id: "format-link-format-default",
-    title: title,
-    contexts: ["link", "selection", "page"]
-  }, () => {
-    chrome.contextMenus.onClicked.addListener((info, tab) => {
-      if (info.menuItemId === "format-link-format-default") {
-        getSelectedText(selection => {
-          gettingOptions(options => {
-            var formatID = options["defaultFormat"];
-            var format = options['format' + formatID];
-            var url = info.linkUrl ? info.linkUrl : info.pageUrl;
-            var title = tab.title;
-            if (info.linkUrl && !selection) {
-              getLinkText(info.linkUrl, text => {
-                var formattedText = formatURL(format, url, title, text);
-                chrome.storage.local.set({
-                  lastCopied: {
-                    url: url,
-                    title: title,
-                    text: text,
-                    formattedText: formattedText
-                  }
-                }, () => {
-                  copyToClipboard(formattedText);
-                });
-              });
-              return;
-            }
-            var text = selection;
-            if (!text) {
-              text = info.selectionText ? info.selectionText : tab.title;
-            }
-            var formattedText = formatURL(format, url, title, text);
-            chrome.storage.local.set({
-              lastCopied: {
-                url: url,
-                title: title,
-                text: text,
-                formattedText: formattedText
-              }
-            }, () => {
-              copyToClipboard(formattedText);
-            });
-          });
-        });
-      }
-    });
+    // Requires the clipboardWrite permission, or a user gesture:
+    document.execCommand("copy");
   });
 }
 
-function getLinkText(url, callback) {
-  chrome.tabs.executeScript({
+async function getLinkText(url) {
+  var results = await chrome.tabs.executeScript({
     code: `
       var links = document.querySelectorAll('a');
       for (var i = 0; i < links.length; i++) {
@@ -80,23 +30,63 @@ function getLinkText(url, callback) {
       }
       text;
     `
-  }, results => {
-    callback(results[0]);
   });
+  return results[0];
 }
 
-function getSelectedText(callback) {
-  chrome.tabs.executeScript({
+async function getSelectedText() {
+  var selection = await chrome.tabs.executeScript({
     code: "window.getSelection().toString();"
-  }, selection => {
-    var text;
-    if (selection && selection[0]) {
-      text = selection[0].trim().replace(/\s+/g, ' ');
-    }
-    callback(text);
   });
+  var text;
+  if (selection && selection[0]) {
+    text = selection[0].trim().replace(/\s+/g, ' ');
+  }
+  return text;
 }
 
-gettingOptions(options => {
-  createContextMenu(options);
-});
+(async function() {
+  try {
+    var options = await gettingOptions();
+    await createContextMenus(options);
+    chrome.contextMenus.onClicked.addListener(async (info, tab) => {
+      if (info.menuItemId.startsWith("format-link-format")) {
+        try {
+          var options = await gettingOptions();
+          var formatID = info.menuItemId.substr("format-link-format".length);
+          if (formatID === "-default") {
+            formatID = options.defaultFormat;
+          }
+          var format = options['format' + formatID];
+          var url = info.linkUrl ? info.linkUrl : info.pageUrl;
+          var title = tab.title;
+          var text = await getSelectedText();
+          if (!text) {
+            if (info.linkUrl) {
+              text = await getLinkText(info.linkUrl);
+            } else {
+              text = title;
+            }
+          }
+          var formattedText = formatURL(format, url, title, text);
+          await chrome.storage.local.set({
+            lastCopied: {
+              url: url,
+              title: title,
+              text: text,
+              formattedText: formattedText
+            }
+          });
+          await copyToClipboard(formattedText);
+          if (formatID !== options.defaultFormat) {
+            await saveDefaultFormat(formatID);
+          }
+        } catch (err) {
+          console.error("FormatLink extension failed to copy URL to clipboard.", err);
+        }
+      }
+    });
+  } catch (err) {
+    console.error("failed to create context menu", err);
+  };
+})();
--- options.js
+++ options.js
@@ -1,33 +1,53 @@
-function restoreOptions() {
-  gettingOptions(options => {
-    for (var i = 1; i <= 9; ++i) {
-      document.getElementById('title'+i).value = options['title'+i] || '';
-      document.getElementById('format'+i).value = options['format'+i] || '';
-    }
-  });
-}
-
-function saveOptions() {
-  var options = {}
+async function restoreOptions() {
+  var options = await gettingOptions();
   for (var i = 1; i <= 9; ++i) {
-    options['title'+i] = document.getElementById('title'+i).value;
-    options['format'+i] = document.getElementById('format'+i).value;
+    document.getElementById('title'+i).value = options['title'+i] || '';
+    document.getElementById('format'+i).value = options['format'+i] || '';
   }
-  chrome.storage.sync.set(options);
+  document.getElementById('createSubmenusCheckbox').checked = options['createSubmenus'];
 }
 
-function restoreDefaults() {
+async function saveOptions(defaultFormatID) {
+  var options;
+  try {
+    options = defaultFormatID ?
+      {'defaultFormat': defaultFormatID} : await gettingOptions();
+    for (var i = 1; i <= 9; ++i) {
+      options['title'+i] = document.getElementById('title'+i).value;
+      options['format'+i] = document.getElementById('format'+i).value;
+    }
+    options['createSubmenus'] = document.getElementById('createSubmenusCheckbox').checked;
+  } catch (err) {
+    console.error("failed to get options", err);
+  }
+  try {
+    await chrome.storage.sync.set(options);
+  } catch (err) {
+    console.error("failed to save options", err);
+  }
+  try {
+    await createContextMenus(options);
+  } catch (err) {
+    console.error("failed to update context menu", err);
+  }
+}
+
+async function restoreDefaults() {
   for (var i = 1; i <= 9; ++i) {
     document.getElementById('title'+i).value = DEFAULT_OPTIONS['title'+i] || '';
     document.getElementById('format'+i).value = DEFAULT_OPTIONS['format'+i] || '';
   }
-  saveOptions();
+  document.getElementById('createSubmenusCheckbox').checked = DEFAULT_OPTIONS['createSubmenus'];
+  await saveOptions(DEFAULT_OPTIONS['defaultFormat']);
 }
 
-function init() {
-  restoreOptions();
-  document.getElementById('saveButton').addEventListener('click', saveOptions);
-  document.getElementById('restoreDefaultsButton').addEventListener('click', restoreDefaults);
+async function init() {
+  await restoreOptions();
+  document.getElementById('saveButton').addEventListener('click', async e => {
+    await saveOptions();
+  });
+  document.getElementById('restoreDefaultsButton').addEventListener('click', async e => {
+    await restoreDefaults();
+  });
 }
-
 document.addEventListener('DOMContentLoaded', init);
